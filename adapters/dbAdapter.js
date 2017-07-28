0166fd6d32bf5cfd645d0ac889330c0328f1054e
Dhruv Prakash <dhruvprakash@Dhruvs-Air.wv.cc.cmu.edu>
2017-07-27 22:44:33 -0400

    reverted because poor latency experienced

--- adapters/dbAdapter.js
+++ adapters/dbAdapter.js
@@ -218,6 +218,7 @@ module.exports = (connectionPool) => {
 
                 }
 
+
                 let queryString = arr.join(' AND ');
 
                 if(!asinPresent && !groupPresent && !keywordPresent) {
@@ -238,26 +239,14 @@ module.exports = (connectionPool) => {
                         if(err !== undefined) {
                             console.log(err);
                         }
-                        if(asinPresent || groupPresent) {
-                            connection.query(`SELECT * FROM products WHERE ${queryString} limit 5`, (err, rows) => {
-                                connection.release();
-                                if(!err) {
-                                    return resolve(rows);
-                                } else {
-                                    return reject();
-                                }
-                            });
-                        } else {
-                            connection.query(`SELECT *, CASE WHEN product_name = '${keyword}' THEN 1 ELSE 0 END AS score, MATCH (product_name, product_description) AGAINST('${keyword}') AS score2 FROM products WHERE MATCH (product_name, product_description) AGAINST('${keyword}') ORDER BY score DESC, score2 DESC limit 5`, (err, rows) => {
-                                connection.release();
-                                if(!err) {
-                                    return resolve(rows);
-                                } else {
-                                    return reject();
-                                }
-                            });
-                        }
-                        
+                        connection.query(`SELECT * FROM products WHERE ${queryString} limit 5`, (err, rows) => {
+                            connection.release();
+                            if(!err) {
+                                return resolve(rows);
+                            } else {
+                                return reject();
+                            }
+                        });
                     });
                 	
                 }
