65088c611b3e8927ce49937f4339603b8dfe64d5
Dhruv Prakash <dhruvprakash@Dhruvs-Air.wv.cc.cmu.edu>
2017-08-03 11:26:57 -0400

    db host and pass change

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 module.exports = {
 	connectionLimit: 1000,
 	//host: 'edis.cfxtkdjyn2xx.us-east-1.rds.amazonaws.com',
-	host: 'dragon-cluster-1.cluster-cfxtkdjyn2xx.us-east-1.rds.amazonaws.com',
-	user: 'dragonslayer',
+	host: 'aurora-cluster.cluster-cfxtkdjyn2xx.us-east-1.rds.amazonaws.com',
+	user: 'godslayer',
 	//user: 'root',
 	//password: 'password',
-	password: 'dragonslayer',
+	password: 'godslayer',
 	database: 'edis'
 }
